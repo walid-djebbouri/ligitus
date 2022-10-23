f0efa5df4006225c117a374c8e6be7ba8bef73f2
walid-digial <walid.djebbouri@digial.dz>
2022-10-23 17:10:26 +0100

    changing $ to user

--- src/app/pages/e-commerce/earning-card/front-side/earning-live-update-chart.component.ts
+++ src/app/pages/e-commerce/earning-card/front-side/earning-live-update-chart.component.ts
@@ -106,7 +106,7 @@ export class EarningLiveUpdateChartComponent implements AfterViewInit, OnDestroy
         backgroundColor: earningLineTheme.tooltipBg,
         borderColor: earningLineTheme.tooltipBorderColor,
         borderWidth: earningLineTheme.tooltipBorderWidth,
-        formatter: params => `$ ${Math.round(parseInt(params.value[1], 10))}`,
+        formatter: params => `User ${Math.round(parseInt(params.value[1] , 10) - 1)}`,
         extraCssText: earningLineTheme.tooltipExtraCss,
       },
       series: [
