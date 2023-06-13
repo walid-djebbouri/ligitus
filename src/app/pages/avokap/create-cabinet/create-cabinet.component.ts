6b255bd9d4bbe7c45465eeed58265d9ba3dff873
walid djebbouri <walid.djebbouri@atos.net>
2023-06-13 16:58:22 +0200

    create organisation

--- src/app/pages/avokap/create-cabinet/create-cabinet.component.ts
+++ src/app/pages/avokap/create-cabinet/create-cabinet.component.ts
@@ -1,10 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NbDialogRef, NbIconLibraries} from '@nebular/theme';
 import {SmartTableData} from '../../../@core/interfaces/common/smart-table';
-import {FormControl, FormGroup, NgForm} from '@angular/forms';
-import data from '../law_domain.json';
+// import data from '../law_domain.json';
 import {Router} from '@angular/router';
-import {ValidationService} from '../../../@core/mock/common/validation.service';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'ngx-create-cabinet',
   templateUrl: './create-cabinet.component.html',
@@ -12,150 +11,87 @@ import {ValidationService} from '../../../@core/mock/common/validation.service';
 })
 export class CreateCabinetComponent implements OnInit {
   @Input() title: string ;
-  cabinet_ref: string;
-  Errors: string[];
-  tel ;
-  tels = [] ;
-  nb_tel = 0 ;
-  cpd;
-  cdps = [] ;
-  nb_cpd = 0 ;
-  donne ;
-  a1 = '';
-  a2 = '';
- selected_wilaya: string;
-  selected: string[];
-  nb = 0;
-  form_template = [];
-  myFormGroup: FormGroup;
-  formTemplate = this.form_template;
+  organisationForm: FormGroup;
 
   constructor(protected ref: NbDialogRef<CreateCabinetComponent> ,
               private service: SmartTableData ,
+              private form: FormBuilder ,
               iconsLibrary: NbIconLibraries ,
-              private router: Router,
-              private Validation: ValidationService) {
+              private router: Router) {
     iconsLibrary.registerFontPack('fa', { packClass: 'fa', iconClassPrefix: 'fa' } )
 ;
   }
   ngOnInit(): void {
-    const group = {} ;
-    this.form_template.forEach(input_template => {
-      group[input_template.label] = new FormControl('');
-    });
-    this.myFormGroup = new FormGroup(group);
-
+    this.createForm();
   }
-  dismiss() {
+
+  private createForm(): void {
+    this.organisationForm = this.form.group({
+      organisation_ref: [null,  [Validators.required] ],
+      join_date: [null, [Validators.required]],
+      legal_name: [ null, [Validators.required]] ,
+      commercial_name: [null, [Validators.required]] ,
+      organisation_type: [null, [Validators.required]],
+      org_short_desc: [null],
+      org_long_desc: [null],
+      nb_customers: [null, [Validators.required]] ,
+      domiciliation: [null, [Validators.required]] ,
+      wilaya: [null, [Validators.required]],
+      wilaya_code: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      tel: this.form.array([]),
+      fax: [null] ,
+      nif: [null, [Validators.required]],
+      rib: [null, [Validators.required]],
+      customers: [null, [Validators.required]],
+
+
+    });
+  }
+
+  get tel() {
+    return this.organisationForm.controls.tel as FormArray;
+  }
+
+  public addPhone(): void {
+    const phone = this.form.group({
+      phone: [null, [Validators.required, Validators.pattern('^0(5|6|7)[0-9]{8,8}')]]
+    });
+    this.tel.push(phone);
+  }
+
+  public deletePhone(index: number): void {
+    this.tel.removeAt(index);
+  }
+
+  public createOrganisation(): void {
+    console.log(
+        this.organisationForm.controls,
+        this.organisationForm.get('organisation_ref').value ,
+        this.organisationForm.get('join_date').value,
+        this.organisationForm.controls.tel.value,
+    );
+  }
+
+  public dismiss() {
     this.ref.close();
   }
 
-  Cabinet_ref (va: Event , index: number) {
-    switch (index) {
-      case 1 :
-        this.a1 =  String(va) ;
-        break ;
-      case 2 :
-        this.a2 = (<HTMLInputElement> va.target).value ;
-        break ;
-    }
-    this.cabinet_ref  = 'CAB' + this.a1 + '_' + this.a2  ;
-  }
-  create_cabinet(): void {
-    this.cdps = [];
-    this.tels = [];
+  Cabinet_ref (va: Event , index: number) {}
 
-    for ( let i = 0 ; i < document.getElementsByName('tele').length; i++ ) {
-      this.tels.push(( <HTMLInputElement> document.getElementsByName('tele')[i] ).value );
-    }
+  create_cabinet(): void {}
 
-    for (let i = 0 ; i < document.getElementsByName('law_catxs').length ; i++  ) {
-      const ladate = new Date() ;
-      this.cpd = {
-        'law_cat' : (<HTMLInputElement>document.getElementsByName('law_catxs')[i]).innerText ,
-        'jur_domain' : (<HTMLInputElement>document.getElementsByName('jur_domain')[i]).innerText,
-        'add_date' : ladate.getDate() + '/' + (ladate.getMonth() + 1) + '/' + ladate.getFullYear(),
-      } ;
-      this.cdps.push(this.cpd);
-    }
+  changeTheme(cat: string) {}
 
-    this.donne = {
-       cabinet_ref : (<HTMLInputElement>document.getElementById('cabinet_refe')).value,
-      legal_name: (<HTMLInputElement>document.getElementById('legal_namee')).value,
-      commercial_name: (<HTMLInputElement>document.getElementById('commercial_namee')).value,
-      cabinet_short_desc: (<HTMLInputElement>document.getElementById('cabinet_short_desce')).value,
-      cabinet_long_desc: (<HTMLInputElement>document.getElementById('cabinet_long_desce')).value,
-      join_date : (<HTMLInputElement>document.getElementById('join_datee')).value  + 'T00:00:00.000Z',
-      domiciliation: (<HTMLInputElement>document.getElementById('domiciliatione')).value,
-      email: (<HTMLInputElement>document.getElementById('emaile')).value,
-      fax: (<HTMLInputElement>document.getElementById('faxe')).value,
-      nif : (<HTMLInputElement>document.getElementById('nife')).value,
-      rib : (<HTMLInputElement>document.getElementById('ribe')).value,
-      tel : this.tels,
-      cabinet_predilection_domains : this.cdps ,
-      wilaya : document.getElementById('wilya').innerText ,
-      wilaya_code: this.selected_wilaya,
-    } ;
-    this.Errors = [];
-    this.Errors = this.Validation.validationCreateCabinet(this.donne);
-    if (this.Errors.length === 0 ) {
-      this.service.create_cabinet(this.donne).then(() => {
-        this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-          this.router.navigate(['/pages/avokap/cabinet-smart-table']);
-          this.ref.close();
-        });
-      }).catch( (error) => {
-        this.Errors = this.Validation.validationForm(error);
-      });
-    }
-  }
-
-  changeTheme(cat: string) {
-    const predilection = [];
-    const i = ((document.getElementsByName('law_catxs')).length);
-    for ( let j = 0 ; j < i ; j++ ) {
-      if (i > 1 ) {
-        predilection.push(( (<HTMLInputElement>document.getElementsByName('jur_domain')[j]).innerText) ) ;
-      }
-    }
-    const categor = (cat.toLowerCase()).replace(/ /g,'');
-    let prd = data[categor] ;
-    predilection.forEach(ele => {
-      prd = prd.filter( pr =>  pr !== ele  ) ;
-    });
-    this.selected = prd;
-  }
-
-  addPredilection(): void {
-    this.nb_cpd++ ;
-    const ls = {
-      'type' : 'select_cat',
-      'label' : 'predilection',
-      'options' : null,
-      'id' :  null,
-    } ;
-    ls.options = data['category'];
-    ls.id = this.nb_cpd ;
-    this.form_template.push(ls);
-  }
+  addPredilection(): void {}
 
 
-  deletePredilection(index: number): void {
-    this.form_template.splice(index, 1);
-    this.nb_cpd-- ;
-  }
+  deletePredilection(index: number): void {}
 
 
-  addTel(): void {
-    const ls = {
-      'label': 'tel',
-    } ;
-    this.form_template.push(ls);
-  }
+  addTel(): void {}
 
-  deleteTel(index: number): void {
-    this.form_template.splice(index, 1);
-  }
+  deleteTel(index: number): void {}
 
   onSubmit(): void {}
 
